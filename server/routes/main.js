d68f8bcc2667911530fee749e223cd8c7f4098c6
Rosa M Perez <rosaperezlarios@gmail.com>
2025-01-27 19:16:19 -0800

    home page built out

---
+++ server/routes/main.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+
+// Routes
+router.get('', (req, res) => {
+    const locals = {
+        title: "NodeJS Blog",
+        description: "Simple blog created with NodeJS, Express, and MongoDb"
+    }
+    // res.send("Hello world")
+    res.render('index', { locals });
+});
+
+router.get('/about', (req, res) => {
+    res.render('about');
+});
+
+module.exports = router;
